6cc328d704a6e82b7d37e75b0ba98389df0975d7
EnriqueMV18 <enriquemv937@gmail.com>
2024-06-03 00:15:26 -0500

    Renovacion 1

--- ms-auth/src/main/java/com/example/msauth/security/JwtProvider.java
+++ ms-auth/src/main/java/com/example/msauth/security/JwtProvider.java
@@ -5,11 +5,14 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class JwtProvider {
     @Value("${jwt.secret}")
     private String secret;
